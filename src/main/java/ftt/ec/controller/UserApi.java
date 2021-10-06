4ae7aa7016c3b4b18d6e2f286b43b3236cd9c0c3
Daniel Carvalho <danielscarvalho@gmail.com>
2021-10-06 00:19:36 -0300

    Fix UserApi

--- src/main/java/ftt/ec/controller/UserApi.java
+++ src/main/java/ftt/ec/controller/UserApi.java
@@ -21,7 +21,6 @@ import ftt.ec.model.UserDao;
 public class UserApi extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-	private HashMap<Integer,User> Users = new HashMap<Integer,User>();
        
     /**
      * @see HttpServlet#HttpServlet()
